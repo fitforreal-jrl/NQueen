4dc4b954acd1cf7ec19eeb16393ce93b80640efb
fitforreal-jrl <71825714+fitforreal-jrl@users.noreply.github.com>
2023-02-27 23:15:16 +0100

    Add loop function in main function

--- NQueen/Program.cs
+++ NQueen/Program.cs
@@ -5,29 +5,31 @@ namespace NQueen
 {
     class Program
     {
-        enum MODE
-        {
-            TEST_MODE,
-            SOLUTION_MODE
-        }
-
         static void Main(string[] args)
         {
-            NQueen queen = new NQueen();
-
-            Console.WriteLine("Please slecte mode (1: solution mode 2: test mode)");
-            var Mode = Console.ReadLine();
-            if (Mode == "1") 
-            {            
-                Console.WriteLine("Please input number of queen to place.");
-                byte bQueen = (byte)(Encoding.ASCII.GetBytes(Console.ReadLine())[0] - 48);               
-                queen.execute(bQueen);
-            }
-            else
+            bool run = true;
+            while (run)
             {
-                Console.WriteLine("Please input number of queen to test the sum of solution. ");
-                byte bQueen = (byte)(Encoding.ASCII.GetBytes(Console.ReadLine())[0] -48);
-                queen.test(bQueen);
+                NQueen queen = new NQueen();
+
+                Console.WriteLine("Please slecte mode (1: solution mode 2: test mode 3: exit)");
+                var Mode = Console.ReadLine();
+                if (Mode == "1")
+                {
+                    Console.WriteLine("Please input number of queen to place.");
+                    byte bQueen = (byte)(Encoding.ASCII.GetBytes(Console.ReadLine())[0] - 48);
+                    queen.execute(bQueen);
+                }
+                else if (Mode == "2")
+                {
+                    Console.WriteLine("Please input number of queen to test the sum of solution. ");
+                    byte bQueen = (byte)(Encoding.ASCII.GetBytes(Console.ReadLine())[0] - 48);
+                    queen.test(bQueen);
+                }
+                else if (Mode == "3")
+                {
+                    run = false;
+                }
             }
             
         }
